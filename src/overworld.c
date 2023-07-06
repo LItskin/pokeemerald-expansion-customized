f5911afc9c83515ccb6ee900e5172a7021f019c4
LItskin <37088810+LItskin@users.noreply.github.com>
2023-07-06 12:19:14 -0500

    Field HM fixes
    Change to use existing function

--- src/overworld.c
+++ src/overworld.c
@@ -3264,7 +3264,7 @@ static bool8 CanLearnFlashInParty(void)
         {
             break;
         }
-        if (!GetMonData(&gPlayerParty[i], MON_DATA_IS_EGG) && CanMonLearnTMHM(&gPlayerParty[i], ITEM_HM05 - ITEM_TM01))
+        if (!GetMonData(&gPlayerParty[i], MON_DATA_IS_EGG) && CanLearnTeachableMove(&gPlayerParty[i], ITEM_HM05 - ITEM_TM01))
         {
             return TRUE;
         }
